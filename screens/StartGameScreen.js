ce5a77f374503b182faa2a690f030fb2768c61da
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-14 20:50:20 -0700

    47b - Setting up basics for the Start Screen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, TextInput } from "react-native";
+
+function StartGameScren() {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+}
+
+export default StartGameScren;
