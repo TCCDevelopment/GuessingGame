9cec74c3fd9adcd1fe74fcd372d586735943485b
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-15 15:23:39 -0700

    Lesson 51 - Stylizing the Buttons
    This stylizes the buttons and gives a basic function for when it is pressed.

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function PrimaryButton({ children }) {
+  function buttonPressed(){
+    console.log('Pressed!!!');
+  }
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={buttonPressed}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    margin: 4, 
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 28, 
+    backgroundColor: '#72063c',
+    elevation: 2
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
+});
