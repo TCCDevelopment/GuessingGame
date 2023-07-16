66a68df43dbd7f9149c0969ebdea4716a49cf820
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-15 19:43:42 -0700

    53b - Used Prettier formatting plugin
    No foundational changes made during this commit.
    
    Used the Prettier plugin to format the code.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,11 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <View style={styles.buttonsContainer}>
         <View style={styles.button}>
           <PrimaryButton>Reset</PrimaryButton>
@@ -33,23 +37,23 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
-    shadowOpacity: 0.25
+    shadowOpacity: 0.25,
   },
-  numberInput:{
-    height: 50, 
+  numberInput: {
+    height: 50,
     width: 50,
-    fontSize: 32, 
-    borderBottomColor: '#ddb52f',
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: "#ddb52f",
     marginVertical: 8,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "center",
   },
   buttonsContainer: {
-    flexDirection: "row"
+    flexDirection: "row",
   },
   button: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
