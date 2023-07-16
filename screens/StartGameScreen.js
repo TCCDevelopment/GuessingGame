db047dbf62a950ca5a904ee1227b1a99f70e0ea3
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-15 19:42:25 -0700

    Lesson 53 - Formatting the input and buttons
    This commit formats the input area to be centered and the buttons to be side by side and the same width.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,8 +6,14 @@ function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
       <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.button}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,6 +22,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -37,5 +45,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  buttonsContainer: {
+    flexDirection: "row"
+  },
+  button: {
+    flex: 1
   }
 });
