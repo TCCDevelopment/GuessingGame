473313e0d778590e3f06bf0e36caa6846e9f5bf4
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-20 20:19:54 -0700

    Lesson 65 - Setting +/- logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, View, Text } from "react-native";
 
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -15,9 +16,22 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
-function GameScreen({userNumber}) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween( minBoundary, maxBoundary, userNumber );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) { // direction => 'lower, 'greater'
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween( minBoundary, maxBoundary, currentGuess );
+    setCurrentGuess(newRndNumber);
+  }
 
   return (
     <View style={styles.screen}>
@@ -25,7 +39,14 @@ function GameScreen({userNumber}) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
