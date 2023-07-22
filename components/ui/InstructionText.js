12f8ad00b6ad863e3fb54267591551d69fc3f157
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-22 16:50:14 -0700

    70d - Implementing the Custom Fonts

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -3,13 +3,18 @@ import { StyleSheet, Text } from "react-native";
 import Colors from "../../constants/colors";
 
 function InstructionText({ children, style }) {
-  return <Text style={[styles.defaultStyle, style, styles.requiredStyle]}>{children}</Text>;
+  return (
+    <Text style={[styles.defaultStyle, style, styles.requiredStyle]}>
+      {children}
+    </Text>
+  );
 }
 
 export default InstructionText;
 
 const styles = StyleSheet.create({
   requiredStyle: {
+    fontFamily: "open-sans",
     color: Colors.accent500,
   },
   defaultStyle: {
