9fd69d7c555c1769a0b2d52385371afb3327bb94
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-22 16:23:46 -0700

    70b - Adding Custom Fonts

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
 
 import Colors from "./constants/colors";
 
@@ -11,6 +12,11 @@ import GameOverScreen from "./screens/GameOverScreen";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(false);
+
+  useFonts({
+    'open-sans': require('/assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
