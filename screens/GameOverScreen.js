abf26edf4626015c33422c5c2abfd665c9190ac1
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-26 20:51:26 -0700

    Lesson 87 - useWindowDimensions
    Learning about dynamic dimensions that can change whenever the code is re-run with the `useWindowDimensions` hook.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,11 @@
-import { Dimensions, StyleSheet, View, Image, Text } from "react-native";
+import {
+  useWindowDimensions,
+  ScrollView,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from "react-native";
 
 import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -6,31 +13,55 @@ import PrimaryButton from "../components/ui/PrimaryButton";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/images/success.png")}
-          style={styles.image}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            source={require("../assets/images/success.png")}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needed{" "}
+          <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlightText}>{userNumber}</Text>.
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed{" "}
-        <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess
-        the number <Text style={styles.highlightText}>{userNumber}</Text>.
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
-const deviceHeight = Dimensions.get('window').height;
+// const deviceWidth = Dimensions.get("window").width;
+// const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     paddingTop: 24,
@@ -39,9 +70,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 200 : 300,
-    height: deviceWidth < 380 ? 200 : 300,
-    borderRadius: deviceWidth < 380 ? 100 : 150,
+    // width: deviceWidth < 380 ? 200 : 300,
+    // height: deviceWidth < 380 ? 200 : 300,
+    // borderRadius: deviceWidth < 380 ? 100 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden",
